1ce578d15fc2302b4c8eb985ba028b461fad9fba
AldinRekic <aldinrekic1@gmail.com>
2023-06-13 15:26:26 +0200

    Author fix

--- src/server/helpers/filterUserForClient.ts
+++ src/server/helpers/filterUserForClient.ts
@@ -1,9 +1,10 @@
-import { User } from "@clerk/nextjs/dist/api";
+import type { User } from "@clerk/nextjs/dist/api";
 
 export const filterUserForClient = (user: User) => {
   return {
     id: user.id,
     username: user.username,
     profileImageUrl: user.profileImageUrl,
+    externalUsername: user.externalAccounts.find((externalAccount) => externalAccount.provider === "oauth_github")?.username || null
   };
 };
